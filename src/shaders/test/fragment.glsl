400c7eb3927a10b4ab293463ae75fb58db7132f2
psaboureau <psaboureau@gmx.fr>
2024-12-27 17:32:33 +0100

    pattern 3_4_5

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -3,5 +3,14 @@ varying vec2 vUv;
 
 void main()
 {
-    gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0);
+    // Pattern 3
+    // float strenght = vUv.x;
+
+    // Pattern 4
+    // float strength = vUv.y;
+
+    // Pattern 5
+    float strenght = 1.0 - vUv.y;
+
+    gl_FragColor = vec4(vec3(strenght), 1.0);
 }
