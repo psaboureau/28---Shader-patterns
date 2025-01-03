d31010190cab9d14af18afb4c4f0753ce094edce
psaboureau <psaboureau@gmx.fr>
2025-01-03 19:54:49 +0100

    qs_01

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -5,6 +5,14 @@ float random(vec2 st)
     return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
 }
 
+vec2 rotate(vec2 uv, float rotation, vec2 mid)
+{
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
+}
+
 void main()
 {
     // Pattern 3
@@ -129,6 +137,19 @@ void main()
     // float strength = 0.015 / distance(lightUv, vec2(0.5));
 
     // Pattern 31
+    // vec2 lightUvX = vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25 );
+    // float lightX = 0.015 / distance(lightUvX, vec2(0.5));
+
+
+    // vec2 lightUvY = vec2(vUv.y * 0.1 + 0.45, vUv.x * 0.5 + 0.25 );
+    // float lightY = 0.015 / distance(lightUvY, vec2(0.5));
+
+    // float strength = lightX * lightY;
+
+
+    // gl_FragColor = vec4(vec3(strength), 1.0);
+
+    // Pattern 32
     vec2 lightUvX = vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25 );
     float lightX = 0.015 / distance(lightUvX, vec2(0.5));
 
